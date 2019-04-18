284d33ca4574572169ad7d67c9cc193ef4b185b5
Paulodir <paulodir.oliveira@aluno.sc.senac.br>
2019-04-17 21:21:50 -0300

    Commit Concluido cadastrar prova

--- application/controllers/Prova.php
+++ application/controllers/Prova.php
@@ -1,16 +1,41 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Prova extends CI_Controller {
-	public function index()
-	{
-		$this->listar();
-                
-	}
-        public function listar(){
+
+    public function index() {
+        $this->listar();
+    }
+
+    public function listar() {
         $this->load->model('Prova_Model', 'pm');
         $data['provas'] = $this->pm->getAll();
         $this->load->view('ListaProvas', $data);
     }
-        
+
+    public function cadastrar() {
+        $this->form_validation->set_rules('nome', 'nome', 'required');
+        $this->form_validation->set_rules('tempo', 'tempo', 'required');
+        $this->form_validation->set_rules('descricao', 'descricao', 'required');
+        $this->form_validation->set_rules('nIntegrantes', 'nIntegrantes', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('FormularioProva');
+        } else {
+            $this->load->model('Prova_Model');
+            $data = array(
+                'nome' => $this->input->post('nome'),
+                'tempo' => $this->input->post('tempo'),
+                'descricao' => $this->input->post('descricao'),
+                'nIntegrantes' => $this->input->post('nIntegrantes')
+            );
+            if($this->Prova_Model->insert($data)){
+                redirect('Prova/listar');
+            }else {
+                redirect('Prova/cadastrar');
+            }
+        }
+    }
+
 }
---
+++ application/views/ListaProvas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lista de Provas</title>
+    </head>
+    <h1>Lista de Provas</h1>
+    <body>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Tempo</th>
+                    <th>Descrição</th>
+                    <th>Nº Integrantes</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>        
+            <tbody>
+                <?php
+                foreach ($provas as $p){
+                    echo '<tr>';
+                    echo '<td>'.$p->nome.'</td>';
+                    echo '<td>'.$p->tempo.'</td>';
+                    echo '<td>'.$p->descricao.'</td>';
+                    echo '<td>'.$p->NIntegrantes.'</td>';
+                    echo '<td>'
+                    .'<a href="">Alterar</a>'
+                    .'<a href="">Deletar</a>'
+                    . '</td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </body>
+</html>
